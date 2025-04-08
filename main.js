31b912b1489d66d35d771542d4690ac13c8e9e7a
Thuyngan <tranttngann@gmail.com>
2025-04-08 16:17:24 +0700

    add navigation bar and change background picture

---
+++ main.js
@@ -0,0 +1,34 @@
+const translations ={
+    en: {
+        <div class="container">
+                <div class="row">
+                    <div class="about-col-1">
+                        <img src="images/intro.png">
+                    </div>    
+                    <div class="about-col-2">
+                        <h1 class="sub-title">Giới thiệu</h1>
+                        <p>Chào các bạn, mình là Ngân. Mình đang sống 1 mình ở Hà Nội với 1 “bé” mèo tên Mie. Mie và mình đã ở cùng nhau được hơn 4 năm rùi. 
+                            Cổ thật sự rất đặc biệt với mình, có thể vì cổ là con mèo đầu tiên và duy nhất mình có từ trc đến giờ. 
+                            Website này đc tạo ra với mục đích lưu giữa kỉ niệm của 2 bọn mình. 
+                            Nó sẽ hơi dài và khá lan man, nên nếu bạn rảnh và cũng là 1 người yêu quý động vật, thì hãy ghé thăm để hiểu rõ hơn về bọn mình hơn nhíe!</p>
+                    </div>    
+                </div>
+            </div>
+    }
+    vi: {
+        <div class="container">
+            <div class="row">
+                <div class="about-col-1">
+                    <img src="images/intro.png">
+                </div>    
+                <div class="about-col-2">
+                    <h1 class="sub-title">Giới thiệu</h1>
+                    <p>Chào các bạn, mình là Ngân. Mình đang sống 1 mình ở Hà Nội với 1 “bé” mèo tên Mie. Mie và mình đã ở cùng nhau được hơn 4 năm rùi. 
+                    Cổ thật sự rất đặc biệt với mình, có thể vì cổ là con mèo đầu tiên và duy nhất mình có từ trc đến giờ. 
+                    Website này đc tạo ra với mục đích lưu giữa kỉ niệm của 2 bọn mình. 
+                    Nó sẽ hơi dài và khá lan man, nên nếu bạn rảnh và cũng là 1 người yêu quý động vật, thì hãy ghé thăm để hiểu rõ hơn về bọn mình hơn nhíe!</p>
+                </div>    
+            </div>
+        </div>
+    }
+}
